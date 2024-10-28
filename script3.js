f21d43e58548fa6674e55d7d5202db44a8bc46de
SalinasJason <jasonsalinas770@gmail.com>
2024-10-27 17:17:41 -0700

    added answer number 3

--- script3.js
+++ script3.js
@@ -9,3 +9,22 @@
 //  # # # #
 // # # # #
 //  # # # #
+
+
+
+
+var board = "";
+var even = " # # # #";
+var odd = "# # # #";
+var i = 1;
+while (i <= 8) {
+
+  if (i % 2 == 0) {
+    console.log(even);
+
+  } else {
+    console.log(odd);
+
+  }
+  var i = i + 1;
+}
